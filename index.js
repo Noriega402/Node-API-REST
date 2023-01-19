abbda93995a2bbb83e411de8e009fd3930460a9a
Noriega402 <d.noriegachajon@gmail.com>
2023-01-18 22:27:41 -0600

    add arguments

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const app = express(); //usando constante de express
 const port = 3000;
 
 // definiendo rutas
-app.get('/', (request, response) => {
-  response.send("Este servidor es creado con express");
+app.get('/', (request, response) => { //request = peticion  response = respuesta
+  response.writeHead(200,{'Content-Type':'text/html'});
+  response.write("<h1>Login</h1>");
+  // response.send("Este servidor es creado con express");
   console.log(request.url);
 })
 
 app.get('/home', (request, response) => {
   response.writeHead(200,{'Content-Type':'text/html'});
-  response.write("<h1>Servidor con Express</h1>");
+  response.write("<h1>Home</h1>");
   console.log(request.url);
 })
 
 app.listen(port, function(){
-  console.log(`Escuchando en: 127.0.0.1:${port}`);
+  console.log(`Escuchando en: 127.0.0.1:${port}/`);
 });
