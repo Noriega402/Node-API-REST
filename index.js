16575b3e59650beaa2da968b68bb1126edee4c72
Noriega402 <d.noriegachajon@gmail.com>
2023-01-21 20:27:37 -0600

    Modify const PORT

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const routerApi = require('./routes');
 const app = express(); //usando constante de express
-const port = 3000;
-
+const PORT = 3000;
 
 app.use(express.json());
-
 // definiendo rutas
 app.get('/', (request, response) => { //request = peticion  response = respuesta
   response.writeHead(200,{'Content-Type':'text/html'});
   response.write("<h1>Primer servidor con Express</h1>");
-  // response.send("Este servidor es creado con express");
-  console.log(request.url);
+  // console.log(request.url);
 });
 
 routerApi(app);
 
-app.listen(port, function(){
-  console.log(`Escuchando en: localhost:${port}/`);
+app.listen(PORT, function(){
+  console.log(`Escuchando en: localhost:${PORT}/`);
 });
