2e317ff66224a7c2d4daf3c28da5095425c4012e
Noriega402 <d.noriegachajon@gmail.com>
2023-01-24 23:10:18 -0600

    add route static 'home' and '404'

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const path = require('path');
 const routerApi = require('./routes');
 const app = express(); //usando constante de express
+
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public'))); //ruta estatica
+
 // definiendo rutas
-app.get('/', (request, response) => { //request = peticion  response = respuesta
-  response.writeHead(200,{'Content-Type':'text/html'});
-  response.write("<h1>Primer servidor con Express</h1>");
-  // console.log(request.url);
-});
+// app.get('/', (request, response) => { //request = peticion  response = respuesta
+//   response.writeHead(200,{'Content-Type':'text/html'});
+//   response.write("<h1>Primer servidor con Express</h1>");
+//   console.log(request.url);
+//   console.log(path.join(__dirname,'\public'));
+// });
 
 routerApi(app);
 
