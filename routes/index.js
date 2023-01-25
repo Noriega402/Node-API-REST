151707ba5d7380b4462696ae03401e75c7465897
Noriega402 <d.noriegachajon@gmail.com>
2023-01-24 23:11:45 -0600

    add route for bad request into url '404.html'

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
-const express = require ('express');
+const express = require('express');
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const loginRouter = require('./login.router');
 const shoppingRouter = require('./shopping.router');
 
-function routerApi(app){
+let root = {root: "C:\\Users\\HP INTEL\\Documents\\Cursos Platzi\\Node-API-REST\\public"};
+
+function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
   router.use('/login', loginRouter);
   router.use('/shopping', shoppingRouter);
+  app.use((request, response, next) => {
+    response.status(404).sendFile('/404.html', root);
+  })
 }
 
 module.exports = routerApi;
