330ed4be6a2c4107a1cd968ce54f4a9d267eb448
Noriega402 <d.noriegachajon@gmail.com>
2023-01-25 19:28:33 -0600

    add argument property on function validate and news validations

--- schemas/product.schemas.js
+++ schemas/product.schemas.js
@@ -1,9 +1,13 @@
 const yup = require('yup');
+const errors = require('./errors/message');
+const regexProduct = /^[a-zA-Z\s]+/gi;
+const regexPrice = /\$[0-9]+\.[0-9]+/gi;
 
-function validate(validation){
+function validate(validation, property) {
   return (request, response, next) => {
     try {
-      validation(request.body);
+      //request[property] es para obtener los datos de body, params o query
+      validation(request[property]);
       next();
     } catch (error) {
       next(error);
@@ -12,14 +16,49 @@ function validate(validation){
   }
 }
 
-function createProductValidation(data){
-  const regexProduct = /^[a-zA-Z]+$/gi;
-  const regexPrice = /\$[0-9]+\.[0-9]+/gi;
-
+function createProductValidation(data) {
   const schema = yup.object().shape({
-    product: yup.string().min(3, "El producto debe tener mas de 3 caracteres").matches(regexProduct).required("Es necesario ingresar un producto"),
-    price: yup.string().matches(regexPrice).required(),
-    description: yup.string().min(10).required(),
+    product: yup
+      .string()
+      .min(3, errors.productMin)
+      .matches(regexProduct, errors.productRegex)
+      .required(errors.required),
+    price: yup
+      .string()
+      .matches(regexPrice, errors.productPrice)
+      .required(),
+    description: yup
+      .string()
+      .min(10, errors.productDescription)
+      .required(errors.required),
+  });
+
+  schema.validateSync(data);
+}
+
+function updateProductValidation(data, property) {
+  const schema = yup.object().shape({
+    product: yup
+      .string()
+      .min(3, errors.productMin)
+      .matches(regexProduct, errors.productRegex),
+    price: yup
+      .string()
+      .matches(regexPrice, errors.productPrice),
+    description: yup
+      .string()
+      .min(10, errors.productDescription),
+  });
+
+  schema.validateSync(data);
+}
+
+function deleteProductValidation(data, property) {
+  const schema = yup.object().shape({
+    id: yup
+      .number(errors.number)
+      .integer(errors.integer)
+      .required(errors.required),
   });
 
   schema.validateSync(data);
@@ -27,5 +66,7 @@ function createProductValidation(data){
 
 module.exports = {
   validate,
-  createProductValidation
+  createProductValidation,
+  updateProductValidation,
+  deleteProductValidation
 };
--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,10 +1,15 @@
 const yup = require('yup');
 const errors = require('./errors/message');
+const regexNames = /^[a-zA-Z]+$/gi;
+const regexMail = /^[a-zA-z0-9_.]+@[a-zA-Z0-9-_]+\.[a-zA-Z]{2,4}/gi;
+const regexUserName = /^[a-zA-Z0-9-_]+/gi;
+const regexCard = /^[0-9]+$/gi;
 
-function validate(validation){
+function validate(validation, property) {
   return (request, response, next) => {
     try {
-      validation(request.body);
+      //request[property] es para obtener los datos de body, params o query
+      validation(request[property]);
       next();
     } catch (error) {
       next(error);
@@ -14,40 +19,77 @@ function validate(validation){
 }
 
 function createUserValidation(data) {
-  const regexNames = /^[a-zA-Z]+$/gi;
-  const regexMail = /^[a-zA-z0-9_.]+@[a-zA-Z0-9-_]+\.[a-zA-Z]{2,4}/gi;
-  const regexUserName = /^[a-zA-Z0-9-_]+/gi;
-  const regexCard = /^[0-9]+$/gi;
-
   const schema = yup.object().shape({
     first_name: yup
-                .string(errors.nameString)
-                .matches(regexNames, errors.nameRegex)
-                .required(errors.required),
+      .string(errors.nameString)
+      .matches(regexNames, errors.nameRegex)
+      .required(errors.required),
     last_name: yup
-              .string(errors.surnameString)
-              .matches(regexNames, errors.surnameRegex)
-              .required(errors.required),
+      .string(errors.surnameString)
+      .matches(regexNames, errors.surnameRegex)
+      .required(errors.required),
     user_name: yup
-              .string(errors.userName)
-              .matches(regexUserName, errors.userNameRegex)
-              .required(errors.required),
+      .string(errors.userName)
+      .matches(regexUserName, errors.userNameRegex)
+      .required(errors.required),
     email: yup
-          .string()
-          .matches(regexMail, errors.emailRegex)
-          .required(errors.required),
+      .string()
+      .matches(regexMail, errors.emailRegex)
+      .required(errors.required),
     direction: yup.string(errors.direction),
     credit_card: yup
-                .string()
-                .min(16, errors.card)
-                .max(16, errors.card)
-                .matches(regexCard),
+      .string()
+      .min(16, errors.card)
+      .max(16, errors.card)
+      .matches(regexCard),
   });
 
   schema.validateSync(data);
 }
 
+function updateUserValidation(data) {
+  const schema = yup.object().shape({
+    first_name: yup
+      .string(errors.nameString)
+      .min(3, errors.nameMin)
+      .matches(regexNames, errors.nameRegex),
+    last_name: yup
+      .string(errors.surnameString)
+      .min(3, errors.surnameMin)
+      .matches(regexNames, errors.surnameRegex),
+    user_name: yup
+      .string(errors.userName)
+      .min(3, errors.userNameMin)
+      .matches(regexUserName, errors.userNameRegex),
+    email: yup
+      .string()
+      .matches(regexMail, errors.emailRegex),
+    direction: yup.string(errors.direction),
+    credit_card: yup
+      .string()
+      .min(16, errors.card)
+      .max(16, errors.card)
+      .matches(regexCard),
+  });
+
+  schema.validateSync(data);
+}
+
+function deleteUserValidation(data){
+  const schema = yup.object().shape({
+    id: yup
+    .number(errors.number)
+    .integer(errors.integer)
+    .required(errors.required),
+  });
+
+  schema.validateSync(data);
+}
+
+
 module.exports = {
   validate,
-  createUserValidation
+  createUserValidation,
+  updateUserValidation,
+  deleteUserValidation
 }
