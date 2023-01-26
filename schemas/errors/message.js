73291fcda4eac960bb75b0845d0646613a3ee214
Noriega402 <d.noriegachajon@gmail.com>
2023-01-25 18:10:34 -0600

    add new message errors to new users

--- schemas/errors/message.js
+++ schemas/errors/message.js
@@ -7,6 +7,8 @@ errors.surnameString = "Tu apellido debe tener solo caracteres";
 errors.userNameRegex = "El username solo puede llevar estos simbolos \"-\" \"_\"";
 errors.userName = "El username debe contener solo caracteres";
 errors.emailRegex = "El email no cumple con los requerimientos";
+errors.direction = "Tu direccion debe tener caracteres validos";
+errors.card = "La tarjeta de debito debe tener 16 digitos";
 errors.required = "El campo no puede quedar vacio";
 
 module.exports = errors;
