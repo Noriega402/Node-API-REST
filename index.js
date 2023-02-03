f9b530c572ea9851344b32eddf6297dd4d5bf4cc
Noriega402 <d.noriegachajon@gmail.com>
2023-02-03 12:37:26 -0600

    add package cors and method cors in index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const routerApi = require('./routes');
 const app = express(); //usando constante de express
+const cors = require('cors');
 
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, '/public'))); //ruta estatica
 
 // definiendo rutas
