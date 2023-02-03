90f998196c4530622b00e35a50c02b6e93e88b28
Noriega402 <d.noriegachajon@gmail.com>
2023-02-03 13:22:49 -0600

    add new route to test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const loginRouter = require('./login.router');
 const shoppingRouter = require('./shopping.router');
+const testRouter = require('./test.router');
 
 const root = {root: "C:\\Users\\HP INTEL\\Documents\\Cursos Platzi\\Node-API-REST\\public"};
 
@@ -13,6 +14,7 @@ function routerApi(app) {
   router.use('/users', usersRouter);
   router.use('/login', loginRouter);
   router.use('/shopping', shoppingRouter);
+  router.use('/test', testRouter);
   app.use((error, request, response, next) => {
     // response.status(404).sendFile('/404.html', root);
     response.json({
