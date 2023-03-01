ee4532fe2f8d863f84480437bf61c567c813ea19
Noriega402 <d.noriegachajon@gmail.com>
2023-02-28 21:01:53 -0600

    add routes with categories

---
+++ routes/categorys.router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const validations = require('../schemas/category.schemas');
+const controller = require('../controllers/category.controller');
+
+router.get('/', controller.getAll); // listado categorias
+router.get('/:id', controller.find); // ver categoria por ID
+router.post('/', validations.validate(validations.createCategoryValidation, "body"), controller.new); //crear nueva categoria
+router.patch('/:id', validations.validate(validations.updateCategoryValidation, "body"), controller.update); //actualizar categoria
+
+router.delete('/:id', validations.validate(validations.deleteCategoryValidation, "params"), controller.delete); //eliminar categoria por ID
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const productsRouter = require('./products.router');
+const categorysRouter = require('./categorys.router');
 const usersRouter = require('./users.router');
 const loginRouter = require('./login.router');
 const shoppingRouter = require('./shopping.router');
 const testRouter = require('./test.router');
 
-const root = {root: "C:\\Users\\HP INTEL\\Documents\\Cursos Platzi\\Node-API-REST\\public"};
-
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
+  router.use('/categories', categorysRouter);
   router.use('/users', usersRouter);
   router.use('/login', loginRouter);
   router.use('/shopping', shoppingRouter);
