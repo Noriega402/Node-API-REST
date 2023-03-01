e4aea158a2ab5ecf0b21aa397b92afcadd1a4a83
Noriega402 <d.noriegachajon@gmail.com>
2023-02-28 21:03:54 -0600

    modify sintax and name methods

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,8 +1,8 @@
-const root = { root: "C:\\Users\\HP INTEL\\Documents\\Cursos Platzi\\Node-API-REST\\public"};
+const path = require('path');
 let data = require('../examples/products.json');
 const controller = {};
 
-controller.getProducts = (request, response) => {
+controller.getAll = (request, response) => {
   const { size } = request.query;
   const getProducts = [];
   const limit = size || 10; //en caso no pasa un valor, se le asigna 10
@@ -42,18 +42,16 @@ controller.getPagination = (request, response, next) => {
   }
 }
 
-controller.findProduct = (request, response) => { //recibe parametros
+controller.find = (request, response) => { //recibe parametros
   const id = Number(request.params.id);
   const search = data.find(product => product.id == id);
 
-  if(search){
-    response.json(search);
-  }else{
-    response.status(404).sendFile("/404.html", root);
-  }
+  search
+    ? response.json(search)
+    : response.status(404).sendFile(path.join(__dirname,'../public/404.html'))
 }
 
-controller.newProduct = (request, response) => {
+controller.new = (request, response) => {
   const body = request.body;
   const ids = data.map(datos => datos.id); //obtener los ids de los productos
   const maxId = Math.max(...ids); // verificar el id mayor
@@ -63,10 +61,11 @@ controller.newProduct = (request, response) => {
     products: body.products,
     price: body.price,
     description: body.description,
+    image: body.image
   }
 
   data = [...data, newProduct];
-  response.json(newProduct);
+  response.status(202).json(newProduct).end();
 }
 
 controller.delete = (request, response) => {
@@ -74,9 +73,10 @@ controller.delete = (request, response) => {
   const index = data.findIndex(item => item.id === id);
   // console.log(data[index]);
   if(index !== -1){
-    response.status(200).json(data[index]).end(); //mostrar producto eliminado
+    data.splice(index, 1);
+    response.status(200).json({message: "Deleted successfully!"}).end(); //mostrar producto eliminado
   }else{
-    response.status(404).sendFile('/404.html', root);
+    response.status(404).json({message: "Bad request"}).end();
   }
 }
 
@@ -88,9 +88,9 @@ controller.update= (request, response) => {
   if(index !== -1){
     const product = data[index];
     data[index] = { ...product, ...body };
-    response.status(202).json(data[index]);
+    response.status(202).json({message: "Updated succesfully!"}).end();
   }else{
-    response.status(406).send("Error de solicutud...").end();
+    response.status(406).json({error: "Bad request"}).end();
   }
 }
 
