70883595901a9a2b398fac48546cad294d4cd14e
Noriega402 <d.noriegachajon@gmail.com>
2023-02-28 21:05:17 -0600

    add validation for categories

---
+++ schemas/category.schemas.js
@@ -0,0 +1,49 @@
+const yup = require('yup');
+const errors = require('./errors/message');
+
+function validate(validation, property){
+    return (request, response, next) => {
+        try {
+            validation(request[property]);
+            next();
+        } catch (error) {
+            next(error);
+            console.log(error);
+        }
+    }
+}
+
+function createCategoryValidation(data){
+    const schema = yup.object().shape({
+        category: yup
+        .string()
+        .required(errors.required),
+    });
+    schema.validateSync(data);
+}
+
+function updateCategoryValidation(data, property){
+    const schema = yup.object().shape({
+        category: yup
+        .string()
+        .required(errors.required),
+    });
+    schema.validateSync(data);
+}
+
+function deleteCategoryValidation(data){
+    const schema = yup.object().shape({
+        id: yup
+        .number(errors.number)
+        .integer(errors.integer)
+        .required(errors.required),
+    });
+    schema.validateSync(data);
+}
+
+module.exports = {
+    validate,
+    createCategoryValidation,
+    deleteCategoryValidation,
+    updateCategoryValidation
+}
