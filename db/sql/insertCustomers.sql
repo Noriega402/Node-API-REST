4147daeb7285895393b25188a32f6c14033e5860
Noriega402 <d.noriegachajon@gmail.com>
2023-03-20 23:25:03 -0600

    agregar sql para insertar datos

---
+++ db/sql/insertCustomers.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.customers( first_name, last_name, phone, created_at, user_id)
+	VALUES ('Melany', 'Salazar', '5676-8990', NOW(), 1),
+	('Daniel', 'Noriega', '4200-7707',NOW(),2),
+	('David', 'Caceres',null,NOW(),3),
+	('Blanca','Milena',null,NOW(),4),
+	('Fernando','Chichilla', null,NOW(),5),
+	('Emily','Hernandez','8745-6732',NOW(),6),
+	('Miltcia','Hernandez',null,NOW(),7),
+	('Leonel','Hernandez',null,NOW(),8);
---
+++ db/sql/insertUsers.sql
@@ -0,0 +1,9 @@
+select *from users;
+INSERT INTO users(id,username,email,password,created_at, role) VALUES (1,'MSalazar23','Msalazar23@gmail.com','$2b$10$Qyj9FfdS/3nx6IKNE5bTIOYQPOg05bE5t.xsvdDPCY33bguv6/n5i','2023-03-20 13:43:47.841-06','customer'),
+(2,'Dnoriega','Dnoriega@gmail.com','$2b$10$Ck7ol7vhwjAfxkcFmNkGYuFCGiiwxXjrKqATP/mHOT1mObzHopaw.','2023-03-20 14:08:46.948-06','customer'),
+(3,'Dcaceres','Dcaceres@gmail.com','$2b$10$J.lwU.2stz8chUj2BYjXgeLMg08ZUeZ.p6Uy8KOyK.O28maJDb01a','2023-03-20 14:09:28.647-06','customer'),
+(4,'Bmilena','Bmilena@gmail.com','$2b$10$bSOJhaW37mRW/ScGZZhRQ.M4gxuvpzSydA3lXdoWd.VIiWLnYjhou','2023-03-20 14:11:44.782-06','customer'),
+(5,'Fchinchilla','Fchinchilla@gmail.com','$2b$10$HzlbJHcaRWivuoE7vHpqC.W/8tq1A7rnp3ZdINrc2q1fItkx0ofFu','2023-03-20 14:12:38.786-06','customer'),
+(6,'Ehernandez','Ehernandez@gmail.com','$2b$10$liHse3CnWoFdh5bczjoA8uKcODa3b6tvf.CBnIE69kiALZyY/GcPy','2023-03-20 14:18:42.992-06','customer'),
+(7,'Mhernandez','Mhernandez@gmail.com','$2b$10$k7mF6EjQIr4oZyxCaX2MY.j6/miB.Vsl6Yu/l/WYY6tF4Gl2eEGTC','2023-03-20 14:19:45.221-06','customer'),
+(8,'Lhernandez','Lhernandez@gmail.com','$2b$10$J899e2w6jp7C7FXA92yAKOlV.7u6vxtR4KyOkcpz74ttC9DorPO5a','2023-03-20 14:20:49.066-06','customer')
